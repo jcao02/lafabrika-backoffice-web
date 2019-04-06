ec6813cad7daa7e867e3bdf4172acdcbcbe7cff3
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-04-06 18:09:39 -0400

    add text-control component

---
+++ app/src/modules/forms/components/text-control.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-text-field :value="this.value" @input="this.emitInput"></v-text-field>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import { AbstractControl } from '../interfaces/abstract-control';
+
+export default class TextControl extends AbstractControl {}
+</script>
