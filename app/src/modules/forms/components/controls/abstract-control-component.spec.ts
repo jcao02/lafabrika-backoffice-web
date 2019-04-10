5216669e4a796082dfd3bc229201477bce361d53
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-04-10 16:20:18 -0400

    inject iObserver and implement Observable interface in AbstractControlComponent

--- app/src/modules/forms/components/controls/abstract-control-component.spec.ts
+++ app/src/modules/forms/components/controls/abstract-control-component.spec.ts
@@ -1,8 +1,12 @@
 import { expect } from 'chai';
-import { shallowMount } from '@vue/test-utils';
+import { replace, fake, SinonSpy, restore, spy } from 'sinon';
+import { shallowMount, mount } from '@vue/test-utils';
 import { AbstractControlComponent } from './abstract-control-component';
 
 describe('AbstractControlComponent', () => {
+  afterEach(() => {
+    restore();
+  });
   describe('state', () => {
     it('should set dirty to true if the input changes its value', () => {
       const wrapper = shallowMount(AbstractControlComponent);
@@ -46,6 +50,101 @@ describe('AbstractControlComponent', () => {
       wrapper.setProps({ value });
       // tslint:disable-next-line: no-unused-expression
       expect(wrapper.vm.state.value).to.eql(value);
+    });
+  });
+  describe('subscribe()', () => {
+    it('should subscribe observer on creation', () => {
+      const iObserver = { update() { /** NOOP */ } };
+      const parent = {
+        name: 'validator-parent',
+        template: `
+          <AbstractControlComponent >
+          </AbstractControlComponent>
+        `,
+        provide: { iObserver },
+        components: { AbstractControlComponent }
+      };
+
+      const wrapper = mount(parent);
+      const child = wrapper.find(AbstractControlComponent).vm;
+
+      expect(( child as any ).observers).to.eql([iObserver]);
+    });
+    it('should not call subscribe if observer is null', () => {
+      const wrapper = shallowMount(AbstractControlComponent);
+      expect(wrapper.vm.observers).to.eql([]);
+    });
+  });
+  describe('unsubscribe()', () => {
+    it('should unsubscribe observer on component destroy', () => {
+      const iObserver = { update() { /** NOOP */ } };
+      const parent = {
+        name: 'validator-parent',
+        template: `
+          <AbstractControlComponent >
+          </AbstractControlComponent>
+        `,
+        provide: { iObserver },
+        components: { AbstractControlComponent }
+      };
+
+      const wrapper = mount(parent);
+      const child = wrapper.find(AbstractControlComponent);
+      child.destroy();
+
+      expect(( child.vm as any ).observers).to.eql([]);
+    });
+    it('should not call unsubscribe if observer is null', () => {
+      const wrapper = shallowMount(AbstractControlComponent);
+      replace(wrapper.vm, 'unsubscribe', fake());
+
+      wrapper.destroy();
+      // tslint:disable-next-line: no-unused-expression
+      expect(( wrapper.vm.unsubscribe as SinonSpy).notCalled).to.be.true;
+    });
+  });
+  describe('notify()', () => {
+    it('should notify on input event', () => {
+      const update = fake();
+      const iObserver = { update };
+      const parent = {
+        name: 'validator-parent',
+        template: `
+          <AbstractControlComponent >
+          </AbstractControlComponent>
+        `,
+        provide: { iObserver },
+        components: { AbstractControlComponent }
+      };
+
+      const wrapper = mount(parent);
+      const child = wrapper.find(AbstractControlComponent);
+
+      const value = 'myValue';
+      ( child.vm as any ).input(value);
+
+      expect(update.calledWith(( child.vm as any ).state));
+    });
+    it('should notify on blur event', () => {
+      const update = fake();
+      const iObserver = { update };
+      const parent = {
+        name: 'validator-parent',
+        template: `
+          <AbstractControlComponent >
+          </AbstractControlComponent>
+        `,
+        provide: { iObserver },
+        components: { AbstractControlComponent }
+      };
+
+      const wrapper = mount(parent);
+      const child = wrapper.find(AbstractControlComponent);
+
+      const value = 'myValue';
+      ( child.vm as any ).blur(value);
+
+      expect(update.calledWith(( child.vm as any ).state));
     });
   });
 });
--- app/src/modules/forms/components/controls/abstract-control-component.ts
+++ app/src/modules/forms/components/controls/abstract-control-component.ts
@@ -1,20 +1,24 @@
-import { Prop, Vue, Component, Emit } from 'vue-property-decorator';
+import { Prop, Vue, Component, Emit, Inject } from 'vue-property-decorator';
 import { ControlState } from '../../classes/controls/control-state';
+import { Observable } from '@/modules/shared/classes/observable';
+import { Observer } from '@/modules/shared/classes/observer';
 
 /**
  * Represents a component that holds a control
  */
 @Component
-export class AbstractControlComponent extends Vue {
+export class AbstractControlComponent extends Vue implements Observable {
   @Prop() error!: boolean;
   @Prop() errorMessages!: string[];
   @Prop() value: any;
+  @Inject({ default: null }) iObserver!: Observer | null;
 
   state: ControlState = {
     value: this.value,
     dirty: false,
     touched: false
   };
+  observers: Observer[] = [];
 
   get touched() {
     return this.state.touched;
@@ -24,8 +28,37 @@ export class AbstractControlComponent extends Vue {
     return this.state.dirty;
   }
 
+  created() {
+    if (this.iObserver) {
+      this.subscribe(this.iObserver);
+    }
+  }
+
   updated() {
     this.state.value = this.value;
+  }
+
+  destroyed() {
+    if (this.iObserver) {
+      this.unsubscribe(this.iObserver);
+    }
+  }
+
+  subscribe(obs: Observer): void {
+    this.observers.push(obs);
+  }
+
+  unsubscribe(obs: Observer): void {
+    const index = this.observers.indexOf(obs);
+    if (index >= 0) {
+      this.observers.splice(index, 1);
+    }
+  }
+
+  notify(): void {
+    this.observers.forEach((obs: Observer) => {
+      obs.update(this.state);
+    });
   }
 
   /**
@@ -38,6 +71,9 @@ export class AbstractControlComponent extends Vue {
     if (this.value !== value && !this.dirty) {
       this.state = { ...this.state, dirty: true };
     }
+
+    // Notify to observers everytime user changes input
+    this.notify();
   }
 
   /**
@@ -50,6 +86,9 @@ export class AbstractControlComponent extends Vue {
     if (!this.touched) {
       this.state = { ...this.state, touched: true };
     }
+
+    // Notify to observers everytime user unfocuses input
+    this.notify();
   }
 
 }
