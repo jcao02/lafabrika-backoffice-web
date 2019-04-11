52c3cc7aa62e8784ee99de2b381f74f58daa7fbd
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-04-11 12:21:25 -0400

    add mixin to authentication form and add env variable for baseURL

--- app/src/modules/authentication/components/authentication-form.vue
+++ app/src/modules/authentication/components/authentication-form.vue
@@ -1,13 +1,22 @@
 <template>
-  <FormWithValidation v-slot="{ valid }">
-    <TextWithValidationControl validators="required"/>
-    <TextWithValidationControl validators="required"/>
+  <FormWithValidation @submit.prevent="onSubmit" v-slot="{ valid }">
+    <TextWithValidationControl validators="required" v-model="form.email"/>
+    <TextWithValidationControl validators="required" v-model="form.password"/>
     <v-btn :disabled="!valid" color="info" type="submit">Entrar</v-btn>
   </FormWithValidation>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import  Component, { mixins } from 'vue-class-component';
+
+// Helper classes
+import { AuthenticationPayload } from '../classes/authentication-payload';
+import { NetworkRequester } from '../../shared/classes/http/network-requester';
+
+// Mixins
+import { TokenAuthenticationManager } from '../mixins/token-authentication-manager';
+
+// Components
 import FormWithValidation from '../../forms/components/forms/form-with-validation.vue';
 import TextWithValidationControl from '../../forms/components/controls/text-with-validation-control.vue';
 
@@ -17,5 +26,17 @@ import TextWithValidationControl from '../../forms/components/controls/text-with
     TextWithValidationControl
   }
 })
-export default class AuthenticationForm extends Vue {}
+export default class AuthenticationForm extends mixins(TokenAuthenticationManager) {
+  form: AuthenticationPayload = {
+    email: '',
+    password: ''
+  };
+
+  async onSubmit(event: Event) {
+    const { email, password } = this.form;
+    const res = await this.authenticate(this.form, { baseURL: process.env.VUE_APP_AUTH_BASE_URL });
+
+    // Do something with res
+  }
+}
 </script>
