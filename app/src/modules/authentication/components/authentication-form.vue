da5fcd30badd1ccb02b84f6a5dd296848a164492
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-04-11 08:59:04 -0400

    add authentication view and authentication-form component

---
+++ app/src/modules/authentication/components/authentication-form.vue
@@ -0,0 +1,22 @@
+<template>
+  <FormWithValidation>
+    <TextWithValidationControl validators="required"/>
+    <TextWithValidationControl validators="required"/>
+    <v-btn color="info" type="submit">Entrar</v-btn>
+  </FormWithValidation>
+</template>
+
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import FormWithValidation from '../../forms/components/forms/form-with-validation.vue';
+import TextWithValidationControl from '../../forms/components/controls/text-with-validation-control.vue';
+
+@Component({
+  components: {
+    FormWithValidation,
+    TextWithValidationControl
+  },
+})
+export default class AuthenticationForm extends Vue {}
+</script>
---
+++ app/src/modules/authentication/views/authentication.vue
@@ -0,0 +1,35 @@
+<template>
+<v-container>
+  <v-layout row justify-center>
+    <v-flex xs4>
+      <!-- Card containing form -->
+      <v-card>
+        <!-- Center card content -->
+        <v-layout column justify-center align-center>
+          <!-- Card title -->
+          <v-card-title primary-title>
+            <h1>Iniciar Sesión</h1>
+          </v-card-title>
+          <!-- Form -->
+          <AuthenticationForm />
+        </v-layout>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import AuthenticationForm from '../components/authentication-form.vue';
+
+@Component({
+  components: {
+    AuthenticationForm
+  },
+})
+export default class Authentication extends Vue {}
+</script>
+
+
+
