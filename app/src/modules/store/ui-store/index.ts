548c69e399d75d2e9b9f3e38d685dd14422c4fe8
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-04-11 18:22:53 -0400

    add getCurrentUser getter in ui-store

--- app/src/modules/store/ui-store/index.ts
+++ app/src/modules/store/ui-store/index.ts
@@ -2,6 +2,7 @@ import { Module } from 'vuex';
 import { UIState } from './state';
 import { RootState } from '../state';
 import { SET_CURRENT_USER, setCurrentUser } from './mutations';
+import { getCurrentUser } from './getters';
 
 export const state: UIState = {
   currentUser: null
@@ -11,5 +12,6 @@ export const uiStore: Module<UIState, RootState> = {
   state,
   mutations: {
     [SET_CURRENT_USER]: setCurrentUser
-  }
+  },
+  getters: { getCurrentUser }
 };
