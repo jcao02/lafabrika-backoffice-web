fe33175bc096403551f8d0ccf59a244bb2d44f35
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-04-11 09:45:50 -0400

    remove unnecessary views and components

--- app/src/router.ts
+++ app/src/router.ts
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
 
 import Authentication from './modules/authentication/views/authentication.vue';
 
@@ -11,22 +10,9 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
       path: '/auth',
       name: 'authentication',
       component: Authentication
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
 });
