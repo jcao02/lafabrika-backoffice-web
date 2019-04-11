43a6bb7c3806169dda72235e27c0017a02a6e4db
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-04-11 14:21:49 -0400

    add control interface for seggregation

--- app/src/modules/forms/components/controls/text-with-validation-control.vue
+++ app/src/modules/forms/components/controls/text-with-validation-control.vue
@@ -13,7 +13,7 @@
 </template>
 
 <script lang="ts">
-import { Prop, Vue, Component } from 'vue-property-decorator';
+import { Prop, Vue, Component, Emit } from 'vue-property-decorator';
 import { AbstractValidator } from '../../classes/validators/abstract-validator';
 import { ValidatorFactory } from '../../classes/factories/validator-factory';
 import { defaultDictionary } from '../../classes/error-messages/default-error-messages';
@@ -21,6 +21,7 @@ import { ValidatorName } from '../../classes/validators/validator-names';
 
 import SingleValueValidator from '../validators/single-value-validator.vue';
 import TextControl from './text-control.vue';
+import { Control } from '../../classes/controls/control';
 
 @Component({
   components: {
@@ -28,7 +29,7 @@ import TextControl from './text-control.vue';
     TextControl
   }
 })
-export default class TextWithValidationControl extends Vue {
+export default class TextWithValidationControl extends Vue implements Control {
   @Prop() value: any;
   @Prop() validators!: string;
   @Prop() label!: string;
@@ -39,6 +40,12 @@ export default class TextWithValidationControl extends Vue {
   get validatorObjects(): AbstractValidator[] {
     return this.validators.split('|').map(v => ValidatorFactory.createValidator(v as ValidatorName));
   }
+
+  @Emit()
+  input(value: any): void {}
+
+  @Emit()
+  blur(value: FocusEvent): void {}
 }
 </script>
 
