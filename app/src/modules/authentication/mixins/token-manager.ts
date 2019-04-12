b3cf228906646d7138dbb12aa60c031744a63fcf
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-04-12 10:39:27 -0400

    add getToken method and docs

--- app/src/modules/authentication/mixins/token-manager.ts
+++ app/src/modules/authentication/mixins/token-manager.ts
@@ -1,22 +1,42 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
 import { decode } from 'jsonwebtoken';
-import { set, remove } from 'js-cookie';
+import { set, remove, get } from 'js-cookie';
 import { User } from '@/modules/shared/classes/resources/user';
 
 const TOKEN_KEY = 'AUTH_TOKEN';
 
 @Component
 export class TokenManager extends Vue {
+  /**
+   * Decodes a JWT token
+   * @param token to be decoded
+   */
   decode(token: string): User | null {
     return decode(token, { json: true }) as User | null;
   }
 
+  /**
+   * Persist the token in the cookies
+   * @param token to be persisted
+   */
   persistToken(token: string) {
     set(TOKEN_KEY, token, {expires: 365});
   }
 
+  /**
+   * Removes token from cookies
+   * @param token to be removed
+   */
   removeToken(token: string) {
     remove(TOKEN_KEY);
+  }
+
+  /**
+   * Gets token from cookies
+   */
+  getToken(): string | null {
+    const tokenOrUndefined = get(TOKEN_KEY);
+    return !!tokenOrUndefined ? tokenOrUndefined : null;
   }
 }
