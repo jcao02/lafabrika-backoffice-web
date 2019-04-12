c8de1ec00db2591f38e8a2f1edbf83929386ed4e
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-04-12 10:24:32 -0400

    add methods to persist and remove token in token manager

--- app/src/modules/authentication/mixins/token-manager.ts
+++ app/src/modules/authentication/mixins/token-manager.ts
@@ -1,11 +1,22 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
 import { decode } from 'jsonwebtoken';
+import { set, remove } from 'js-cookie';
 import { User } from '@/modules/shared/classes/resources/user';
+
+const TOKEN_KEY = 'AUTH_TOKEN';
 
 @Component
 export class TokenManager extends Vue {
   decode(token: string): User | null {
     return decode(token, { json: true }) as User | null;
+  }
+
+  persistToken(token: string) {
+    set(TOKEN_KEY, token, {expires: 365});
+  }
+
+  removeToken(token: string) {
+    remove(TOKEN_KEY);
   }
 }
