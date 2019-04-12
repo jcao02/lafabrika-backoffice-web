e467109f3f43fd70a6471ff1b38ce83aa45386e5
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-04-12 15:28:29 -0400

    home -> backoffice

--- app/src/modules/backoffice/views/backoffice.vue
+++ app/src/modules/backoffice/views/backoffice.vue
@@ -18,7 +18,7 @@ import Component from 'vue-class-component';
 import Vue from 'vue';
 
 @Component
-export default class Home extends Vue {}
+export default class Backoffice extends Vue {}
 
 </script>
 
