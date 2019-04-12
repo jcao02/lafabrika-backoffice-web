d13bb847440a5b899835f9f6ab75d10df0474ec7
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-04-12 17:52:39 -0400

    persist authentication in cookies (#16)
    * install js-cookies to handle token persistence
    
    * update package.json
    
    * add methods to persist and remove token in token manager
    
    * persist token after authentication in form
    
    * add getToken method and docs
    
    * add data-initial state from token
    
    * add ui initial state from token
    
    * add signed-in flow and sign-out action (#17)
    
    * add tool bar and sub-routing in home component
    
    * home -> backoffice
    
    * improve mutations types in authentication form
    
    * remove token arg from removeToken method
    
    * add sign-out functionality
    
    * improve authetication styles (#18)
    
    * improve sign-in card styles and add image
    
    * improve form button styles
    
    * improve general spacing

--- app/src/modules/authentication/components/authentication-form.vue
+++ app/src/modules/authentication/components/authentication-form.vue
@@ -3,7 +3,7 @@
     <v-alert :value="generalError" type="error">{{ generalErrorMsg }}</v-alert>
     <TextWithValidationControl validators="required" type="email" v-model="form.email" label="Correo electrónico"/>
     <TextWithValidationControl validators="required" type="password" v-model="form.password" label="Contraseña"/>
-    <v-btn :disabled="!valid" color="info" type="submit">Entrar</v-btn>
+    <v-btn class="submit-btn" :disabled="!valid" color="#5B788E" dark type="submit">Entrar</v-btn>
   </FormWithValidation>
 </template>
 
@@ -24,8 +24,8 @@ import FormWithValidation from '../../forms/components/forms/form-with-validatio
 import TextWithValidationControl from '../../forms/components/controls/text-with-validation-control.vue';
 
 // Mutations
-import { SET_CURRENT_USER } from '../../store/ui-store/mutations';
-import { ADD_USERS } from '../../store/data-store/mutations';
+import { SET_CURRENT_USER, SetCurrentUserPayload } from '../../store/ui-store/mutations';
+import { ADD_USERS, AddUsersPayload } from '../../store/data-store/mutations';
 
 @Component({
   components: {
@@ -34,8 +34,8 @@ import { ADD_USERS } from '../../store/data-store/mutations';
   }
 })
 export default class AuthenticationForm extends mixins(TokenAuthenticationManager, TokenManager) {
-  @Mutation(SET_CURRENT_USER) setCurrentUser: any;
-  @Mutation(ADD_USERS) addUsers: any;
+  @Mutation(SET_CURRENT_USER) setCurrentUser!: (payload: SetCurrentUserPayload) => void;
+  @Mutation(ADD_USERS) addUsers!: (payload: AddUsersPayload) => void;
   form: AuthenticationPayload = {
     email: '',
     password: ''
@@ -54,6 +54,7 @@ export default class AuthenticationForm extends mixins(TokenAuthenticationManage
         this.addUsers({ users: [userOrNull] });
         this.setCurrentUser({ userId: userOrNull.id });
 
+        this.persistToken(token);
         this.$router.push({ path: '/' });
       } else {
         const errorMsg = 'Algo inesperado ocurrió, vuelve a intentar más tarde';
@@ -79,3 +80,10 @@ export default class AuthenticationForm extends mixins(TokenAuthenticationManage
 
 }
 </script>
+
+<style scoped>
+.submit-btn {
+  width: 100%;
+  margin: 16px 0 0 0;
+}
+</style>
--- app/src/modules/store/ui-store/initial-state.ts
+++ app/src/modules/store/ui-store/initial-state.ts
@@ -1,5 +1,22 @@
 import { UIState } from './state';
+import { TokenManager } from '@/modules/authentication/mixins/token-manager';
 
-export const uiState: UIState = {
-  currentUser: null
-};
+/**
+ * Gets the initial state
+ */
+function getInitialState(): UIState {
+  let currentUser: string | null;
+
+  const tokenManager = new TokenManager();
+  const tokenOrNull = tokenManager.getToken();
+  if (tokenOrNull) {
+    const userOrNull = tokenManager.decode(tokenOrNull);
+    currentUser = !!userOrNull ? userOrNull.id : null;
+  } else {
+    currentUser = null;
+  }
+
+  return { currentUser };
+}
+
+export const uiState: UIState = getInitialState();
