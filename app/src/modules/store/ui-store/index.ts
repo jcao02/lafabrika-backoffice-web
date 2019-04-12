4882ec0b1e9e08baf6c7fede8679e9ba1ebd830c
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-04-12 10:40:34 -0400

    add ui initial state from token

--- app/src/modules/store/ui-store/index.ts
+++ app/src/modules/store/ui-store/index.ts
@@ -3,13 +3,10 @@ import { UIState } from './state';
 import { RootState } from '../state';
 import { SET_CURRENT_USER, setCurrentUser } from './mutations';
 import { getCurrentUser } from './getters';
-
-export const state: UIState = {
-  currentUser: null
-};
+import { uiState } from './initial-state';
 
 export const uiStore: Module<UIState, RootState> = {
-  state,
+  state: uiState,
   mutations: {
     [SET_CURRENT_USER]: setCurrentUser
   },
