31637d0af9d7dab2a6fe22a524c5fef60b217832
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-04-13 14:14:10 -0400

    use lafabrika theme

--- app/src/plugins/vuetify.ts
+++ app/src/plugins/vuetify.ts
@@ -2,6 +2,10 @@ import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 import 'vuetify/src/stylus/app.styl';
 
+import theme from './lafabrika-theme';
+
+
 Vue.use(Vuetify, {
-  iconfont: 'md'
+  iconfont: 'md',
+  theme
 });
