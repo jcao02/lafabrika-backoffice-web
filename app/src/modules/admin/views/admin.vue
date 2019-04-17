3f6ed9b97539ccaa5413580f57cff41d351c52aa
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-04-16 20:21:06 -0400

    add admin module

---
+++ app/src/modules/admin/views/admin.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="admin">
+    <h1>ADMIN</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+export default class Admin extends Vue {}
+</script>
