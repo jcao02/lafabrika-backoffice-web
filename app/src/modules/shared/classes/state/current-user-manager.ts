73837999ec90dab07f33fccb2af70a3e9d82473f
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-04-16 21:58:02 -0400

    separate current-user-manager mixin

--- app/src/modules/shared/classes/state/current-user-manager.ts
+++ app/src/modules/shared/classes/state/current-user-manager.ts
@@ -1,17 +1,14 @@
-import Vue from 'vue';
-import Component from 'vue-class-component';
 import store from '@/modules/store';
 
 /**
  * Class to manage everything related to the current user.
  */
-@Component
-export class CurrentUserManager extends Vue {
-  isSignedIn(): boolean {
+export class CurrentUserManager {
+  static isSignedIn(): boolean {
     return !!store.getters.getCurrentUser;
   }
 
-  isAdmin(): boolean {
-    return this.isSignedIn() && store.getters.getCurrentUser.role === 'admin';
+  static isAdmin(): boolean {
+    return CurrentUserManager.isSignedIn() && store.getters.getCurrentUser.role === 'admin';
   }
 }
