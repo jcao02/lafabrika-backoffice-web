8db02ad319c3c19370b61268dae0c4bf7c9d85ec
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-04-17 18:18:04 -0400

    add user-new module and route

---
+++ app/src/modules/user-new/views/user-new.vue
@@ -0,0 +1,11 @@
+<template>
+<h1>USER NEW</h1>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component
+export default class UserNew extends Vue {}
+</script>
