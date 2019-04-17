7c5d6726afae491c26f40273771e65d1d791a65c
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-04-17 18:50:02 -0400

    add basic template form in user-new view

---
+++ app/src/modules/user-new/components/user-new-form.vue
@@ -0,0 +1,38 @@
+<template>
+  <FormWithValidation ref="form" @submit.prevent="onSubmit" v-slot="{ valid }">
+    <v-alert :value="generalError" type="error">{{ generalErrorMsg }}</v-alert>
+    <TextWithValidationControl validators="required" type="email" v-model="form.email" label="Correo electrónico"/>
+    <TextWithValidationControl validators="required" type="password" v-model="form.password" label="Contraseña"/>
+    <v-btn class="submit-btn" :disabled="!valid" type="submit" color="primary">Enviar</v-btn>
+  </FormWithValidation>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+import { FormWithValidation, TextWithValidationControl } from '@/modules/forms';
+
+@Component({
+  components: {
+    FormWithValidation,
+    TextWithValidationControl
+  }
+})
+export default class UserNewForm extends Vue {
+  form = {
+    email: '',
+    password: ''
+  };
+
+  generalError = false;
+  generalErrorMsg = '';
+}
+</script>
+
+<style scoped>
+.submit-btn {
+  width: 100%;
+  margin: 16px 0 0 0;
+}
+</style>
--- app/src/modules/user-new/views/user-new.vue
+++ app/src/modules/user-new/views/user-new.vue
@@ -1,11 +1,45 @@
 <template>
-<h1>USER NEW</h1>
+  <v-container>
+    <v-layout column justify-center align-center align-content-center>
+      <!-- Card containing form -->
+      <v-card class="card" width="400">
+        <!-- Center card content -->
+        <v-layout column align-center fill-height>
+          <v-img width="100%" :src="require('@/assets/guernica.jpg')"></v-img>
+          <!-- Card title -->
+          <v-card-title primary-title>
+            <h1>Nuevo Usuario</h1>
+          </v-card-title>
+          <!-- Form -->
+          <v-card-text class="form-container">
+            <UserNewForm />
+          </v-card-text>
+        </v-layout>
+      </v-card>
+    </v-layout>
+  </v-container>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
 
-@Component
+import { UserNewForm } from '../components';
+
+@Component({
+  components: {
+    UserNewForm
+  }
+})
 export default class UserNew extends Vue {}
 </script>
+
+<style scoped>
+.card {
+  margin-top: 32px;
+  padding-bottom: 32px;
+}
+.form-container {
+  padding: 0 32px;
+}
+</style>
