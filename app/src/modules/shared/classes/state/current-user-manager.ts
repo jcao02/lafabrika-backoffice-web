4db13d831466c2db1cf4d6d85dfcb167865115fb
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-04-16 20:41:24 -0400

    convert user manager in mixin

--- app/src/modules/shared/classes/state/current-user-manager.ts
+++ app/src/modules/shared/classes/state/current-user-manager.ts
@@ -1,9 +1,12 @@
+import Vue from 'vue';
+import Component from 'vue-class-component';
 import store from '@/modules/store';
 
 /**
  * Class to manage everything related to the current user.
  */
-export class CurrentUserManager {
+@Component
+export class CurrentUserManager extends Vue {
   isSignedIn(): boolean {
     return !!store.getters.getCurrentUser;
   }
