2a814243456698342f9cf6861837d4ddacbfb2a9
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-04-16 20:45:05 -0400

    add admin button to backoffice component

--- app/src/modules/backoffice/views/backoffice.vue
+++ app/src/modules/backoffice/views/backoffice.vue
@@ -4,10 +4,10 @@
     <v-toolbar-title> La Fábrika </v-toolbar-title>
     <v-spacer></v-spacer>
     <v-toolbar-items>
+      <v-btn v-if="isAdmin()" to="/admin" flat>Panel Administración</v-btn>
       <v-btn @click="onSignOut" flat>Salir</v-btn>
     </v-toolbar-items>
   </v-toolbar>
-
   <!-- Children routes -->
   <router-view/>
 </div>
@@ -16,12 +16,20 @@
 <script lang="ts">
 import Vue from 'vue';
 import Component, { mixins } from 'vue-class-component';
-import { Mutation } from 'vuex-class';
+import { Mutation, Getter } from 'vuex-class';
+
+// Mixins
+import { CurrentUserManager } from '@/modules/shared/classes/state/current-user-manager';
 import { TokenManager } from '@/modules/authentication/mixins/token-manager';
+
+// Store Mutarions
 import { SET_CURRENT_USER, SetCurrentUserPayload } from '@/modules/store/ui-store/mutations';
 
+// Helper classes
+import { User } from '@/modules/shared/classes/resources/user';
+
 @Component
-export default class Backoffice extends mixins(TokenManager) {
+export default class Backoffice extends mixins(TokenManager, CurrentUserManager) {
   @Mutation(SET_CURRENT_USER) setCurrentUser!: (payload: SetCurrentUserPayload) => void;
 
   /**
