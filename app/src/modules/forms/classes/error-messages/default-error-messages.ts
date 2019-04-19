a6e498cdf094995d7776738a6e1ef3f4f03b953b
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-04-19 18:51:38 -0400

    change dictionary type to accept functions as return type

--- app/src/modules/forms/classes/error-messages/default-error-messages.ts
+++ app/src/modules/forms/classes/error-messages/default-error-messages.ts
@@ -5,5 +5,5 @@ export const defaultDictionary: ErrorMessagesDictionary = {
   default: 'Hay un error en este campo',
   [ValidatorName.REQUIRED]: 'Campo requerido',
   [ValidatorName.EMAIL]: 'Email inválido',
-  [ValidatorName.MINLENGTH]: 'Debe tener al menos X caracteres'
+  [ValidatorName.MINLENGTH]: (length) => `Debe tener al menos ${length} caracteres`
 };
