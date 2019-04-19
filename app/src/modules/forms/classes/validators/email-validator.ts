b649d9036bfd69fc2d7f13f7d5245085c788e15d
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-04-19 18:23:01 -0400

    fix email validation condition

--- app/src/modules/forms/classes/validators/email-validator.ts
+++ app/src/modules/forms/classes/validators/email-validator.ts
@@ -8,6 +8,6 @@ export class EmailValidator implements AbstractValidator {
   validate(value: any): EmailValidationError | null {
     // tslint:disable-next-line: max-line-length
     const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return emailRegex.test(value) ? new EmailValidationError() : null ;
+    return !emailRegex.test(value) ? new EmailValidationError() : null ;
   }
 }
