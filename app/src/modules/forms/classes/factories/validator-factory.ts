3e1be6c38c7f9e7a4caf15e578638e8687c1fc98
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-04-19 20:40:02 -0400

    add match-value validator and error

--- app/src/modules/forms/classes/factories/validator-factory.ts
+++ app/src/modules/forms/classes/factories/validator-factory.ts
@@ -1,4 +1,11 @@
-import { AbstractValidator, RequiredValidator, ValidatorName, EmailValidator, MinLengthValidator } from '../validators';
+import {
+  AbstractValidator,
+  RequiredValidator,
+  EmailValidator,
+  MinLengthValidator,
+  MatchValueValidator,
+  ValidatorName
+} from '../validators';
 
 export class ValidatorFactory {
   static createValidator(type: ValidatorName, ...args: any): AbstractValidator {
@@ -9,6 +16,8 @@ export class ValidatorFactory {
         return new EmailValidator();
       case ValidatorName.MINLENGTH:
         return new MinLengthValidator(args);
+      case ValidatorName.MATCHVALUE:
+        return new MatchValueValidator(args);
       default:
         throw new Error(`Invalid type ${type}`);
     }
