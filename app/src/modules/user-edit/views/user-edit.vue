4d18c3a383ac34affea09111165ee0a5430c11aa
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-04-20 23:08:23 -0400

    add back button in user-new and user-edit forms

--- app/src/modules/user-edit/views/user-edit.vue
+++ app/src/modules/user-edit/views/user-edit.vue
@@ -1,23 +1,28 @@
 <template>
-  <v-container>
-    <v-layout column justify-center align-center align-content-center>
-      <!-- Card containing form -->
-      <v-card class="card" width="400">
-        <!-- Center card content -->
-        <v-layout column align-center fill-height>
-          <v-img width="100%" :src="require('@/assets/guernica.jpg')"></v-img>
-          <!-- Card title -->
-          <v-card-title primary-title>
-            <h1>Editar Usuario</h1>
-          </v-card-title>
-          <!-- Form -->
-          <v-card-text class="form-container">
-            <UserEditForm :user="user"/>
-          </v-card-text>
-        </v-layout>
-      </v-card>
-    </v-layout>
-  </v-container>
+  <div class="user-edit">
+    <v-btn class="back-btn" fab flat color="primary" to="/admin">
+      <v-icon>arrow_back</v-icon>
+    </v-btn>
+    <v-container>
+      <v-layout column justify-center align-center align-content-center>
+        <!-- Card containing form -->
+        <v-card class="card" width="400">
+          <!-- Center card content -->
+          <v-layout column align-center fill-height>
+            <v-img width="100%" :src="require('@/assets/guernica.jpg')"></v-img>
+            <!-- Card title -->
+            <v-card-title primary-title>
+              <h1>Editar Usuario</h1>
+            </v-card-title>
+            <!-- Form -->
+            <v-card-text class="form-container">
+              <UserEditForm :user="user"/>
+            </v-card-text>
+          </v-layout>
+        </v-card>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script lang="ts">
@@ -66,6 +71,10 @@ export default class UserEdit extends Vue {
 </script>
 
 <style scoped>
+.back-btn {
+  margin-top: 16px;
+  margin-left: 316px;
+}
 .card {
   margin-top: 32px;
   padding-bottom: 32px;
