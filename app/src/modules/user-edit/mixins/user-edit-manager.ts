ef65e05a03ea0014a412e69bf18093128bdd8dfe
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-04-28 21:54:46 +0200

    add updatePassword method in user-edit-manager

--- app/src/modules/user-edit/mixins/user-edit-manager.ts
+++ app/src/modules/user-edit/mixins/user-edit-manager.ts
@@ -22,4 +22,19 @@ export class UserEditManager extends Vue {
     const path = `/admin/users/${id}`;
     return this.$http.patch(path, payload, options);
   }
+
+  /**
+   * Updates a user's password
+   * @param id of th user
+   * @param payload with new password
+   * @param options of the request
+   */
+  updatePassword(
+    id: string,
+    payload: { currentPassword: string, newPassword: string },
+    options?: RequestOptions
+  ): Promise<Response<void>> {
+    const path = `/admin/user-private-informations/${id}`;
+    return this.$http.patch(path, payload, options);
+  }
 }
